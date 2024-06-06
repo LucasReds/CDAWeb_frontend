ee0849d9e3a507a1b763d3cbdd5fd15ad9c095ad
Lucas Rojas <lucas.rojas@uc.cl>
2024-06-05 22:18:46 -0400

    Agreagar info

--- tank-wars/src/pages/About.jsx
+++ tank-wars/src/pages/About.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import './About.css';
 
 function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>This is a game about tank battles.</p>
+    <div className="about-container">
+      <div className="about-content">
+        <h1 className="about-title">About Us</h1>
+        <h2 className="about-subtitle">Nuestro Equipo</h2>
+        <div className="team-member">
+          <h3>Domingo Venegas</h3>
+          <p>Bio: Hola Mundo</p>
+        </div>
+        <div className="team-member">
+          <h3>Cristian Gandarillas</h3>
+          <p>Bio: Hola Mundo</p>
+        </div>
+        <div className="team-member">
+          <h3>Lucas Rojas</h3>
+          <p>Bio: Hola Mundo</p>
+        </div>
+        <h2 className="about-subtitle">Curso</h2>
+        <p className="course-info">IIC2513 - Tecnologías y Aplicaciones Web.</p>
+      </div>
     </div>
   );
 }
--- tank-wars/src/pages/Rules.jsx
+++ tank-wars/src/pages/Rules.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
-import playButton from '../components/playButton';
-import standardButton from '../components/standardButton';
-import './Home.css';
+import './Rules.css';
 
 function Rules() {
-  const handleClick = () => {
-    alert('Button Clicked!');
-  };
   return (
     <div style={{ position: 'relative' }}>
-      <div className="title">
+      <div className="sub-title">
         <h1>Tank Wars</h1>
       </div>
-      <h2 className='centered-text sub-title'>Rules</h2>
+      <div className="rules-container">
+        <ol className="rules-list">
+          <li>
+            <strong>Objective:</strong> Destroy the tanks of your adversaries and be the last one standing!
+          </li>
+          <li>
+            <strong>Turn Order:</strong> Players take turns to:
+            <ol>
+              <li>Buy more resources</li>
+              <li>Move their tank</li>
+              <li>Fire a projectile</li>
+            </ol>
+          </li>
+          <li>
+            Players must calculate the positioning and direction of their tank, the power of their shot, the type of ammunition to use, and other strategic decisions.
+          </li>
+          <li>
+            Based on performance in the matches, players will earn rewards that can be used to upgrade their tanks (statistics and ammunition).
+          </li>
+        </ol>
+      </div>
     </div>
   );
 }
