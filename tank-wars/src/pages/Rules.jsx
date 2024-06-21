cb2e29fc15a3bb29d15ef54224d7c21489e1378b
Cristian Gandarillas <cgandarillas2@uc.cl>
2024-06-20 23:40:01 -0400

    store fetch proyectil

--- tank-wars/src/pages/Rules.jsx
+++ tank-wars/src/pages/Rules.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Rules.css';
+import Store from '../components/store';
 
 function Rules() {
+  const [isStoreOpen, setStoreOpen] = useState(true); // Initially true for testing
+
+  const handleCloseStore = () => {
+    console.log("Store closed");
+    setStoreOpen(false); // Update state to close the store
+  };
+
   return (
     <div style={{ position: 'relative' }}>
       <div className="sub-title">
@@ -28,6 +36,7 @@ function Rules() {
           </li>
         </ol>
       </div>
+      <Store isOpen={isStoreOpen} onClose={handleCloseStore} />
     </div>
   );
 }
